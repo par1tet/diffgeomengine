55125cb4b9b2c2fe1eb7a5984f091406681b1677
par1tet <mankova486@gmail.com>
2026-03-15 22:58:38 +0300

    fix

--- core/diffgeomeng/classes/compute/partical_differentiation.hpp
+++ core/diffgeomeng/classes/compute/partical_differentiation.hpp
@@ -14,21 +14,21 @@ double diffBy(std::function<double(const Point<N>&)> func,
 /////////// IMPLEMENTATION /////////////
 
 template <size_t N>
-double diffBy(std::function<double(const std::vector<double>&)> func, const std::vector<double>& point, int p_index, double dx){
+double diffBy(std::function<double(const Point<N>&)> func, const Point<N>& point, int p_index, double dx){
     if(func == nullptr){
         return 0;
     }
 
-    if (p_index < 0 || p_index >= point.size()) {
+    if (p_index < 0 || p_index >= N) {
         throw std::out_of_range("Index out of bounds in diffBy");
     }
     
-    std::array<double, N> copyPoint(point);
+    Point<N> copyPoint = Point<N>(point);
 
-    copyPoint[p_index] += dx;
+    copyPoint.x[p_index] += dx;
     double dPlus = func(copyPoint);
 
-    copyPoint[p_index] -= 2*dx;
+    copyPoint.x[p_index] -= 2*dx;
     double dMinus = func(copyPoint);
 
     return (dPlus - dMinus)/(2*dx);
