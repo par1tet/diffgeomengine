3d12c7dfb59fcc5948d775bd13ca7cce027d4668
par1tet <mankova486@gmail.com>
2026-03-26 15:13:13 +0300

    fix

--- core/diffgeomeng/classes/diff/Manifold.hpp
+++ core/diffgeomeng/classes/diff/Manifold.hpp
@@ -134,7 +134,7 @@ State<N> Manifold<N>::normalizeVelocity(State<N> state, double normal, bool isLo
 
 template <size_t N>
 std::array<double, N> Manifold<N>::normalizeVector(std::array<double, N> vector, Point<N> point, double normal, bool isLogging){
-    std::array<double, N> newVector{};
+    std::array<double, N> newVector = vector;
     std::array<std::array<double, N>, N> g = this->metric->getMatrixAtPoint(point);
 
     double length2 = 0.0;
