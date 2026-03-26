b1033195020fa59c1bd75c4b8497483799032ddb
par1tet <mankova486@gmail.com>
2026-03-26 14:11:59 +0300

    fix: fix normalize for manifold

--- core/diffgeomeng/classes/diff/Manifold.hpp
+++ core/diffgeomeng/classes/diff/Manifold.hpp
@@ -81,6 +81,19 @@ State<N> Manifold<N>::normalizeVelocity(State<N> state, double normal, bool isLo
     // if null-geodesic
     if(std::abs(normal) < 1e-14)
     {
+        double spatial = 0.0;
+
+        for(int i = 1; i < N; ++i){
+            for(int j = 1; j < N; ++j){
+                spatial += g[i][j] * newState.v0[i] * newState.v0[j];
+            }
+        }
+
+        double gtt = g[0][0];
+
+        if(std::abs(gtt) < 1e-14)
+            throw std::runtime_error("Metric g_tt ~ 0");
+
         double a = g[0][0];
         double b = 0.0;
         double c = spatial;
