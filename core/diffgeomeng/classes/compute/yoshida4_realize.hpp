cf906d7cad0be2c774d39b5f6663801ca054752d
par1tet <mankova486@gmail.com>
2026-03-27 16:22:45 +0300

    feat: add yoshida4

---
+++ core/diffgeomeng/classes/compute/yoshida4_realize.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include<diffgeomeng/classes/compute/verlet_realize.hpp>
+
+/////////// IMPLEMENTATION /////////////
+
+// in state need x and p |!| not u
+template<size_t N>
+inline State<N> computeYoshida4(Hamiltonian<N>* ham,const State<N>& initStateP, double dx){
+    checkCorrectState<N>(initStateP);
+
+    State<N> integrateState = initStateP;
+
+    const double w1 = 1.35120719195966;
+    const double w2 = -1.70241438391932;
+
+    integrateState = computeVerlet(ham, state, w1 * dt);
+    integrateState = computeVerlet(ham, state, w2 * dt);
+    integrateState = computeVerlet(ham, state, w1 * dt); 
+
+    return integrateState;
+}
