ba315e184e5c74ea69fafd4b17a4f7f52243e876
par1tet <mankova486@gmail.com>
2026-03-27 16:36:03 +0300

    fix: change dt -> dx

--- core/diffgeomeng/classes/compute/yoshida4_realize.hpp
+++ core/diffgeomeng/classes/compute/yoshida4_realize.hpp
@@ -14,9 +14,9 @@ inline State<N> computeYoshida4(Hamiltonian<N>* ham,const State<N>& initStateP, 
     const double w1 = 1.35120719195966;
     const double w2 = -1.70241438391932;
 
-    integrateState = computeVerlet(ham, integrateState, w1 * dt);
-    integrateState = computeVerlet(ham, integrateState, w2 * dt);
-    integrateState = computeVerlet(ham, integrateState, w1 * dt); 
+    integrateState = computeVerlet(ham, integrateState, w1 * dx);
+    integrateState = computeVerlet(ham, integrateState, w2 * dx);
+    integrateState = computeVerlet(ham, integrateState, w1 * dx); 
 
     return integrateState;
 }
