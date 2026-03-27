a7267186a861263710a773a568108a979c3edce1
par1tet <mankova486@gmail.com>
2026-03-27 16:27:11 +0300

    fix: fix yoshida4

--- core/diffgeomeng/classes/compute/yoshida4_realize.hpp
+++ core/diffgeomeng/classes/compute/yoshida4_realize.hpp
@@ -14,9 +14,9 @@ inline State<N> computeYoshida4(Hamiltonian<N>* ham,const State<N>& initStateP, 
     const double w1 = 1.35120719195966;
     const double w2 = -1.70241438391932;
 
-    integrateState = computeVerlet(ham, state, w1 * dt);
-    integrateState = computeVerlet(ham, state, w2 * dt);
-    integrateState = computeVerlet(ham, state, w1 * dt); 
+    integrateState = computeVerlet(ham, integrateState, w1 * dt);
+    integrateState = computeVerlet(ham, integrateState, w2 * dt);
+    integrateState = computeVerlet(ham, integrateState, w1 * dt); 
 
     return integrateState;
 }
